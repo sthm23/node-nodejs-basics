749420afc6fbc62c7177d3637e00a3f81534d929
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-11-28 14:11:04 +0500

    feat: done fs copy and create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+import * as fsPromises from 'fs/promises';
+import * as fs from 'fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const path = _dirname + '/files';
+const copyPath = _dirname + '/files_copy';
+
 const copy = async () => {
-    // Write your code here 
-};
+    fs.stat(path, async (err, stat) => {
+        if(err === null) {
+
+            fs.stat(copyPath, async (copyErr, re) => {
+                if(copyErr === null) {
+                    throw new Error('FS operation failed');
+                } else if (copyErr.code === 'ENOENT') {
+                    await fsPromises.mkdir(copyPath);
+                    const files = await fsPromises.readdir(path);
+                    for (const file of files) {
+                        const source = join(path, file);
+                        const destination = join(copyPath, file);
+                        await fsPromises.copyFile(source, destination);
+                    }
+                } else {
+                    throw new Error(copyErr);
+                }
+            })
+
+        } else if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw new Error(err);
+        }
+    })
+}
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import * as fsPromises from 'fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const path = _dirname + '/files/fresh.txt';
+const text = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
-};
+    try {
+        const stat = await fsPromises.stat(path);
+        if(stat.isFile()) throw new Error('FS operation failed');
+    } catch (err) {
+        if(err.code === 'ENOENT') return await fsPromises.writeFile(path, text);
+        return console.log(err);
+    }
+}
 
 await create();
