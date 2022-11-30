eb2d45dc082cc8da3e7c9a89a23d9dad3ecdc3db
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-11-30 16:12:16 +0500

    feat: done streams block

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import {stdout} from 'node:process';
+import {createReadStream} from 'node:fs';
+import {join, dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+const path = join(__dirname, 'files');
+const sourcePath = join(path, 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readable = createReadStream(sourcePath);
+    // let str = '';
+    // readable.on('data', (chunk)=>{
+        // str += chunk.toString();
+    // });
+
+    readable.pipe(stdout);
+
+    readable.on('end', ()=>{
+        readable.unpipe(stdout);
+        // console.log(str)
+    });
+
+    readable.on('error', (err)=>{
+        throw new Error(err);
+    })
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import {Transform, pipeline} from 'stream';
+import {stdin, stdout} from 'node:process';
+
+const readable = stdin;
+const writable = stdout;
+
 const transform = async () => {
-    // Write your code here 
+    const transform = new Transform({
+        transform(chunk, enc, cb) {
+            const chunkStr = chunk.toString().trim();
+            const newStr = chunkStr.split('').reverse().join('');
+            // this.push(newStr + '\n');
+            // cb(new Error('oops'), newStr+'\n')
+            cb(null, newStr+'\n')
+        }
+    });
+
+    pipeline(
+        readable,
+        transform,
+        writable,
+        err=>{console.log(err);
+        }
+    );
 };
 
 await transform();
