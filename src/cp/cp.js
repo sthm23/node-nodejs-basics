56335508a0fffdb5015f0c7b80ef930dd333bb90
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-12-05 16:02:50 +0500

    feat: done task child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,21 +1,21 @@
 import { spawn } from 'node:child_process';
+import { stdin, exit } from 'node:process';
 
-// const spawnChildProcess = async (args) => {
-//     // Write your code here
-// };
+const spawnChildProcess = async (args) => {
+      const ls = spawn('node', ['src/cp/files/script.js', ...args]);
+      ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+      ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      });
+      ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        exit()
+      });
+      stdin.on('data', (ch)=>{
+        ls.stdin.write(ch);
+      })
+};
 
-// spawnChildProcess();
-
-const ls = spawn('node', ['src/wt/main.js', '/usr']);
-
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+spawnChildProcess(['test']);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,12 +1,14 @@
-const args = process.argv.slice(2);
+import {argv, stdin, stdout, exit} from 'node:process'
+
+const args = argv.slice(2);
 
 console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+    if (chunkStringified.includes('CLOSE')) exit(0);
+    stdout.write(`Received from master process: ${chunk.toString()}\n`)
 };
 
-process.stdin.on('data', echoInput);
+stdin.on('data', echoInput);
